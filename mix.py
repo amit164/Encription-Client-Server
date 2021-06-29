39645f98854f7fac5db7a98fb22c1d4a0de44d08
‪shilat givati‬‏ <72948258+ShilatGivati@users.noreply.github.com>
2021-06-30 01:05:19 +0300

    Add files via upload

--- mix.py
+++ mix.py
@@ -1,1 +1,75 @@
 # Shilat Givati, 206377038, Amit Sharabi, 323784298
+
+import random
+import socket
+import sys
+
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import threading
+import time
+
+global messages, mutex
+if len(sys.argv) < 2:
+    print("Invalid input")
+    exit(1)
+
+
+def sender():
+    global messages
+    while True:
+        time.sleep(60)  # wait for next round
+        mutex.acquire()
+        random.shuffle(messages)  # randomizes the messages list
+        for m in messages:
+            sender_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sender_sock.connect((m[0], m[1]))
+            sender_sock.send(m[2])
+            sender_sock.close()
+        messages = []
+        mutex.release()
+
+
+def get_server_port():
+    ips_file = open("ips.txt", "r")
+    servers_info = ips_file.readlines()
+    ips_file.close()
+    server_info = servers_info[int(sys.argv[1]) - 1]
+    server_info.replace('\n', '')
+    return int(server_info.split(' ')[1])
+
+
+def get_secret_key():
+    sk_file = open("sk" + str(int(sys.argv[1])) + ".pem", "rb")
+    secret_key_file = sk_file.read()
+    sk_file.close()
+    return secret_key_file
+
+
+mix_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mix_socket.bind(('', get_server_port()))  # listen to any IP
+mix_socket.listen(500)
+messages = []
+mutex = threading.Lock()
+thread = threading.Thread(target=sender, args=())
+thread.start()
+
+while True:
+    client_socket, client_address = mix_socket.accept()
+    secret_key = serialization.load_pem_private_key(get_secret_key(), password=None, backend=default_backend())
+    text = secret_key.decrypt(client_socket.recv(8192),
+                              padding.OAEP(mgf=padding.MGF1(algorithm=SHA256()), algorithm=SHA256(), label=None))
+    dest_ip = ""
+    for i in range(4):
+        dest_ip += str(int(text[i]))
+        if i != 3:
+            dest_ip += '.'
+    dest_port = int.from_bytes(text[4:6], byteorder='big')  # port -> message
+    text = text[6:]  # without port
+    mutex.acquire()
+    messages.append([dest_ip, dest_port, text])  # insert the to the list
+    mutex.release()
+
+    client_socket.close()
--- sender.py
+++ sender.py
@@ -33,7 +33,7 @@ def encrypt_data(plaintext, server):
 def send_messages(messages, servers_info):
     for message in messages:
         to_send = message.split(" ")
-
+        to_send[6].replace('\n', '')
         # parsing message
         password = str.encode(to_send[3])
         salt = str.encode(to_send[4])
@@ -67,26 +67,27 @@ def send_messages(messages, servers_info):
         path.reverse()
 
         ciphertext = encrypt_data(plaintext, path[0])
-
+        server_ip, server_port = servers_info[int(path[0]) - 1].split(" ")
         path = path[1:]
         for i in range(len(path)):
-            server_ip, server_port = servers_info[i].split(" ")
-            server_port = int(server_port).to_bytes(2, 'big')
+            server_port_bytes = int(server_port).to_bytes(2, 'big')
             temp_ip = b''
-            for num in server_ip:
+            ip_part = server_ip.split(".")
+            for num in ip_part:
                 temp_ip += int(num).to_bytes(1, 'big')
-            server_ip = temp_ip
-
-            plaintext = server_ip + server_port + ciphertext
+            server_ip_bytes = temp_ip
+            plaintext = server_ip_bytes + server_port_bytes + ciphertext
             ciphertext = encrypt_data(plaintext, path[i])
+
+            server_ip, server_port = servers_info[int(path[i]) - 1].split(" ")
 
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(server_ip, server_port)
+            s.connect((server_ip, int(server_port)))
             s.send(ciphertext)
             s.close()
         except ConnectionError:
-            print("There was a problem connecting to server")
+            print("Connection Error")
 
 
 x = sys.argv[1]
@@ -95,7 +96,7 @@ file = open("messages" + x + ".txt")
 messages = file.readlines()
 file.close()
 
-servers_file = open("ips.txt")
+servers_file = open("ips.txt", "r")
 servers_info = servers_file.readlines()
 servers_file.close()
 
@@ -108,17 +109,18 @@ for message in messages:
     m = message.split(" ")
     round = m[2]
     if int(round) not in messages_by_round:
-        messages_by_round[int(round)] = [message[:-1]]
+        messages_by_round[int(round)] = [message.replace('\n', '')]
     else:
-        messages_by_round[int(round)].append(message[:-1])
+        messages_by_round[int(round)].append(message.replace('\n', ''))
 
 messages_by_round = sorted(messages_by_round.items())
 
 first_round = time.time()
 round = 0
 for message in messages_by_round:
+    time.sleep(60 * (message[0] - round))
+    round = message[0]
     if message[0] == round:
         send_messages(message[1], servers_info)
-        round += 1
-    while time.time() <= 60 * round + first_round:
-        continue
+    # while time.time() <= 60 * round + first_round:
+    #     continue
