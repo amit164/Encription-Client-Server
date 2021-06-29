d3dffea9c0dd733250ccfae1503f69c4e0f703f0
‪shilat givati‬‏ <72948258+ShilatGivati@users.noreply.github.com>
2021-06-29 11:41:41 +0300

    receiver file

--- receiver.py
+++ receiver.py
@@ -1,1 +1,39 @@
 # Shilat Givati, 206377038, Amit Sharabi, 323784298
+
+import socket
+import time
+import sys
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.hashes import SHA256
+
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+from datetime import datetime
+
+if len(sys.argv) < 4:
+    print("Invalid input")
+    exit(1)
+
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = int(sys.argv[3])
+receiver_socket.bind(('0.0.0.0', port))  # listen to any IP
+receiver_socket.listen(500)  # TODO enough?
+
+password = sys.argv[1]
+salt = sys.argv[2]
+key_derivation_function = PBKDF2HMAC(algorithm=SHA256(), length=32, salt=salt.encode(),
+                                     iterations=100000, backend=default_backend())
+key = Fernet(base64.urlsafe_b64encode(key_derivation_function).derive(password.encode()))
+
+# receive messages
+while True:
+    client_socket, client_address = receiver_socket.accept()
+    ciphertext = client_socket.recv(8192)  # TODO p3?
+    origin_msg = key.decrypt(ciphertext).decode("utf-8")
+
+    # print the message and time
+    print(origin_msg, time.strftime("%H:%M:%S", time.localtime(time.time())))
+    client_socket.close()
